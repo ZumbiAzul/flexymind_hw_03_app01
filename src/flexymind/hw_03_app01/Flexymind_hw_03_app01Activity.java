84b7d23f8fabcb009ddefd745a7d110764bbb2c1
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-04 22:00:30 +0400

    Added intents to be done by buttons, that is modified main.xml and Flexymind_hw_03_app01Activity.java

--- src/flexymind/hw_03_app01/Flexymind_hw_03_app01Activity.java
+++ src/flexymind/hw_03_app01/Flexymind_hw_03_app01Activity.java
@@ -1,7 +1,9 @@
 package flexymind.hw_03_app01;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Flexymind_hw_03_app01Activity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,17 @@ public class Flexymind_hw_03_app01Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void newActivity(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, NewActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void closeActivity(View view) {
+        // Do something in response to button
+        finish();
     }
 }
